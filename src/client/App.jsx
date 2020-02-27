0565cad9db4bbe3053c5bbac666a2d7ea3cb5057
Jess <jessicatanhc@gmail.com>
2020-02-27 16:39:12 +0800

    Add form

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+class TodoList extends React.Component {
+  render() {
+    var displayList = this.props.values.map(list=> {
+        return <p>{list}</p>
+    });
+    return (
+      <div>
+        {displayList}
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+            todo : "",
+            todoList: []
+        }
+  }
+  create() {
+    let newTodo = "";
+    newTodo = this.state.todoList;
+    newTodo.push(this.state.todo);
+    this.setState({todo:""});
+  }
+  changeHandler(event){
+    this.setState({todo:event.target.value});
+    console.log("change", event.target.value);
+  }
+
   render() {
     return (
       <div>
         Welcome.
+        <TodoList values={this.state.todoList} />
+        <div className="item">
+        <input onChange={(event)=>{this.changeHandler(event); } }value ={this.state.todo}/>
+        </div>
+        <button onClick={()=>{this.create()}}>Create</button>
       </div>
     );
   }
